f96424f979f5071e7cd92891a0deff4ab0a3f7db
zhangwei <zhangwei@antiy.cn>
2019-08-07 16:16:45 +0800

    feat(book): add cover to book

--- src/component/book.component/book.compnent.js
+++ src/component/book.component/book.compnent.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import "./book.component.css";
+import './book.component.css';
 
 export function BookComponent(props) {
-    return (<div className="perspective">
-        <div className="book-wrap">
-            <div className="page book-content">end</div>
-            <div className="page book-content book-content1">-------</div>
-            <div className="page book-content book-content2">----</div>
-            <div className="page book-content book-content3">---</div>
-            <div className="page book-cover">{props.name}</div>
+  return (
+    <div className="perspective">
+      <div className="book-wrap">
+        <div className="page book-content">end</div>
+        <div className="page book-content book-content1">-------</div>
+        <div className="page book-content book-content2">----</div>
+        <div className="page book-content book-content3">---</div>
+        <div className="page book-cover">
+          <img src={props.image} alt="" />
         </div>
-    </div>)
+      </div>
+    </div>
+  );
 }
